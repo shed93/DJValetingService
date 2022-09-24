29b060d8d6012f34a33ba931abfbf646fdbcf86a
Liam Adshead <azurepheonix@hotmail.co.uk>
2022-09-24 18:48:17 +0100

    added seed admin

--- DJValetingService/Data/ApplicationDbContext.cs
+++ DJValetingService/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using DJValetingService.Models;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -12,6 +13,24 @@ namespace DJValetingService.Data
             : base(options, operationalStoreOptions)
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //a hasher to hash the password before seeding the user to the db
+            var hasher = new PasswordHasher<ApplicationUser>();
+            //Seeding the User to AspNetUsers table
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = "8b1f1931-99b6-4efd-9127-fd6fc1274968", // primary key
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    PasswordHash = hasher.HashPassword(null, "Pa55w0rd!"),
+                    Email = "admin@test.com",
+                    EmailConfirmed = true,
+                }
+            );
         }
     }
 }
--- DJValetingService/Data/Migrations/20220924174650_added_seed_admin.Designer.cs
+++ DJValetingService/Data/Migrations/20220924174650_added_seed_admin.Designer.cs
@@ -4,6 +4,7 @@ using DJValetingService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DJValetingService.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220924174650_added_seed_admin")]
+    partial class added_seed_admin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
